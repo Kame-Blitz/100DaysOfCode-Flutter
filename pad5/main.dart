6b01cd7fb8c1e1efa7c8036b4106683f36e3e98d
Kame-Blitz <108971502+Kame-Blitz@users.noreply.github.com>
2023-03-09 09:59:20 +0000

    .

---
+++ pad5/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'ans.dart';
+import 'qs.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _ind = 0;
+  var qs = [
+    {
+      'qst': 'color',
+      'anst': [
+        'red',
+        'blue',
+        'green',
+      ]
+    },
+    {
+      'qst': 'animals',
+      'anst': [
+        'rabbit',
+        'blue bird',
+        'giraffe',
+      ]
+    },
+    {
+      'qst': '69',
+      'anst': [
+        'Yes',
+        'No',
+      ]
+    },
+  ];
+
+  void ans() {
+    setState(() {
+      _ind++;
+    });
+    debugPrint('$_ind');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('App')),
+        body: Column(
+          children: [
+            Qs(qs[_ind]),
+            Ans(ans),
+            Ans(ans),
+            Ans(ans),
+          ],
+        ),
+      ),
+    );
+  }
+}
